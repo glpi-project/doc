d5d150ed670cf6442e56edaa0406c836e8dbb558
Johan Cwiklinski <jcwiklinski@teclib.com>
2017-05-10 13:55:18 +0200

    Use fancybox for images zoom; fixes #12

--- source/conf.py
+++ source/conf.py
@@ -129,7 +129,7 @@ todo_include_todos = True
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'default'
+html_theme = 'glpi'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -139,6 +139,7 @@ html_theme = 'default'
 
 # Add any paths that contain custom themes here, relative to this directory.
 # html_theme_path = []
+html_theme_path = ['_templates']
 
 # The name for this set of Sphinx documents.
 # "<project> v<release> documentation" by default.
